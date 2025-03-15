b22742c4fb080821b2ed9e399496b60583a3822d
Irming1109 <77323794+Irming1109@users.noreply.github.com>
2025-03-15 11:46:55 +0100

    Update __init__.py

--- custom_components/pwmngt/__init__.py
+++ custom_components/pwmngt/__init__.py
@@ -1,8 +1,19 @@
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up integrationen."""
-    hass.states.async_set(f"sensor.{DOMAIN}", "Energy manager")
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Opsæt integrationen via YAML (valgfrit)."""
     return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Opsæt integrationen via UI."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Håndter fjernelse af en integration via UI."""
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
