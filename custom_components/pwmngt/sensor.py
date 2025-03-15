4b15f9c7ccbb8f09ee610ed5beffd8f1b8315c86
Irming1109 <77323794+Irming1109@users.noreply.github.com>
2025-03-15 11:49:24 +0100

    Update sensor.py

--- custom_components/pwmngt/sensor.py
+++ custom_components/pwmngt/sensor.py
@@ -3,7 +3,7 @@ from .const import DOMAIN, DEFAULT_NAME
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up sensoren."""
-    async_add_entities([PwMngt()], True)
+    async_add_entities([pwmngt()], True)
 
 class HelloWorldSensor(Entity):
     """En simpel sensor, der returnerer en fast værdi."""
