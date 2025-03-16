f98ffbd6b38e35c965790b05e7a76ef98ac9d500
Irming1109 <77323794+Irming1109@users.noreply.github.com>
2025-03-16 11:07:34 +0100

    Update __init__.py

--- custom_components/pwmngt/__init__.py
+++ custom_components/pwmngt/__init__.py
@@ -12,11 +12,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Opsæt integrationen via UI."""
     hass.data.setdefault(DOMAIN, {})
 
-    # Brug den korrekte metode til at tilføje platformen
+    # Videresend setup af sensoren
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Håndter fjernelse af en integration via UI."""
-    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    return await hass.config_entries.async_forward_entry_unload(entry, ["sensor"])
