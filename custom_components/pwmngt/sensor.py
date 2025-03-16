f54615b89acbd236fde43b0e6ea3f93508fb5b90
Irming1109 <77323794+Irming1109@users.noreply.github.com>
2025-03-16 13:06:17 +0100

    Update sensor.py

--- custom_components/pwmngt/sensor.py
+++ custom_components/pwmngt/sensor.py
@@ -1,32 +1,27 @@
 from homeassistant.helpers.entity import Entity
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-import logging
-
-_LOGGER = logging.getLogger(__name__)
-
-from .const import DOMAIN, DEFAULT_NAME
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
-    """Opsæt sensoren fra en konfigurationsindgang."""
-    _LOGGER.debug("PWMngt: async_setup_entry kaldt!")
-    async_add_entities([PwmngtSensor()])
+from homeassistant.helpers.device_registry import DeviceInfo
+from .const import DOMAIN
 
 class PwmngtSensor(Entity):
-    """En simpel sensor med en fast værdi."""
+    """En simpel sensor for PWMngt."""
 
     def __init__(self):
-        """Initialisering."""
-        self._attr_name = "PWMngt Sensor"
+        self._attr_name = "PWMngt Hello World"
+        self._attr_unique_id = "pwmngt_hello_world"
         self._attr_state = "Hello world"
-        _LOGGER.debug("PWMngtSensor oprettet med state: %s", self._attr_state)
 
     @property
-    def name(self):
-        """Returnerer navnet på sensoren."""
-        return self._attr_name
+    def device_info(self) -> DeviceInfo:
+        """Returnerer enhedsoplysninger, så integrationen vises i UI."""
+        return DeviceInfo(
+            identifiers={(DOMAIN, "pwmngt_device")},  # Unikt ID for enheden
+            name="PWMngt Device",
+            manufacturer="Mit Eget Firma",
+            model="PWMngt Model 1",
+            sw_version="1.0.0",
+        )
 
     @property
     def state(self):
-        """Returnerer sensorens værdi."""
+        """Returnerer sensortilstanden."""
         return self._attr_state
