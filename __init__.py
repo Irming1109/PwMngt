2f3be4b85346bd7c08fb1e5ed8596ff2665026ae
Irming1109 <77323794+Irming1109@users.noreply.github.com>
2025-03-16 12:57:12 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from homeassistant.core import HomeAssistant
-
-from .const import DOMAIN
-
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up integrationen."""
-    hass.states.async_set(f"sensor.{DOMAIN}", "Energy manager")
-    return True
