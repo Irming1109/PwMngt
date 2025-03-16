99045fd05cc308d453331463a1d119e326d34840
Irming1109 <77323794+Irming1109@users.noreply.github.com>
2025-03-16 11:11:12 +0100

    Update sensor.py

--- custom_components/pwmngt/sensor.py
+++ custom_components/pwmngt/sensor.py
@@ -1,11 +1,15 @@
 from homeassistant.helpers.entity import Entity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 from .const import DOMAIN, DEFAULT_NAME
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     """Opsæt sensoren fra en konfigurationsindgang."""
+    _LOGGER.debug("PWMngt: async_setup_entry kaldt!")
     async_add_entities([PwmngtSensor()])
 
 class PwmngtSensor(Entity):
@@ -15,6 +19,7 @@ class PwmngtSensor(Entity):
         """Initialisering."""
         self._attr_name = "PWMngt Sensor"
         self._attr_state = "Hello world"
+        _LOGGER.debug("PWMngtSensor oprettet med state: %s", self._attr_state)
 
     @property
     def name(self):
